918d450e3e6b58a41b37fc92ac1ba4783870c33a
Chris Miceli <git@miceli.net.au>
2014-07-27 18:29:48 +1000

    bug fixed

--- sheetyfs.js
+++ sheetyfs.js
@@ -1,7 +1,7 @@
 
 SheetyFS = (function(){
     SheetyFS = {};
-    SheetyFS.index = {};
+    SheetyFS.index = undefined;
     SheetyFS.key = "";
     SheetyFS.uname = "";
 
@@ -16,6 +16,7 @@ SheetyFS = (function(){
             dataType:"json",
             url: "https://spreadsheets.google.com/feeds/cells/"+this.key+"/od6/public/values?alt=json&min-row=1&max-row=1",
             success: function(data){
+                that.index = {};
                 for (i = 0; i < data.feed.entry.length; i++){
                     user = data.feed.entry[i].content.$t.split(',');
                     that.index[user[0]] = user[1];
@@ -27,16 +28,29 @@ SheetyFS = (function(){
 
     };
 
-    SheetyFS.getRow = function(){
+    SheetyFS.getRow = function(min, max){
+        var that = this;
+        if (that.index === undefined) {
+            that.getIndex();
+        }
         lol = [];
+        url = "https://spreadsheets.google.com/feeds/cells/"+this.key+"/od6/public/values?alt=json&min-row="+this.index[this.uname]+"&max-row="+this.index[this.uname];
+        if (min !== undefined){
+            url = url+"&min-col="+min;
+        }
+        if (max !== undefined){
+            url = url+"&max-col="+max;
+        }
         $.ajax({
             dataType: "json",
-            url: "https://spreadsheets.google.com/feeds/cells/"+this.key+"/od6/public/values?alt=json&min-row="+this.index[this.uname]+"&max-row="+this.index[this.uname],
+            url: url,
             success: function(data){
+                if (data.feed.entry === undefined) {
+                    return;
+                }
                 for (i = 0; i < data.feed.entry.length; i++){
                     lol.push(data.feed.entry[i].content.$t);
                 }
-
             },
             async: false,
         });
